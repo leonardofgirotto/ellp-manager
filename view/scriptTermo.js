922d58b11ddb97f4059fe264e291d2c1b3d9bab0
Rmoraess <148163039+Rmoraess@users.noreply.github.com>
2024-09-01 17:35:20 -0300

    Termo de Adesao
    javascript não completo

---
+++ view/scriptTermo.js
@@ -0,0 +1,11 @@
+document.querySelector('.view-btn').addEventListener('click', () => {
+    alert('Ver Completo clicked');
+});
+
+document.querySelector('.print-btn').addEventListener('click', () => {
+    alert('Imprimir clicked');
+});
+
+document.querySelector('.show-more-btn').addEventListener('click', () => {
+    alert('Mostrar mais clicked');
+});
